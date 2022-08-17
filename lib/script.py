3f06a5deb3e9d746182e23231dc1cd4fb926750c
Taylor Sturgill <phiandery@gmail.com>
2022-08-16 19:09:24 -0700

    Completed HW

--- lib/script.py
+++ lib/script.py
@@ -1,3 +1,30 @@
 from capitals import states
+import random
 
-print(states)
+def state_capitals():
+    random.shuffle(states)
+
+    print("Welcome to the State Capital Game!")
+
+    right=0
+    wrong=0
+    for i in range(0, len(states)):
+        Input=input(f"Enter the capital of {states[i]['name']}: ")
+
+        if Input == states[i]['capital']:
+            right+=1
+            print(f'Good job, you have {right} right')
+        else:
+            wrong+=1
+            print(f'Oops, you have {wrong} wrong')
+
+    print(f"Right: {right}")
+    print(f"wrong: {wrong}")
+
+    game_over=input(f"Would you like to play again?: ")
+    if game_over=='yes':
+        return state_capitals()
+    else:
+        print('Game Over')
+        
+state_capitals()
